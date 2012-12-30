cc48cf00f1be73019e55258e9970ac9d149c5127
Bert Balcaen <bert@rekalldesign.com>
2012-12-30 21:37:19 +0100

    Fixing sorting of events in admin interface.

--- rd_event.php
+++ rd_event.php
@@ -45,7 +45,9 @@ class Rd_Event extends Rd_Cms{
 
 		add_filter('manage_rd_event_posts_columns', array(&$this, 'manage_rd_event_posts_columns'));
 		add_action('manage_rd_event_posts_custom_column', array(&$this, 'manage_rd_event_posts_custom_column'), 10, 2);
-		add_filter('manage_edit_rd_event_sortable_columns', array(&$this, 'manage_edit_rd_event_sortable_columns'));
+		add_filter('manage_edit-rd_event_sortable_columns', array(&$this, 'manage_edit_rd_event_sortable_columns'));
+		add_filter('request', array(&$this, 'column_orderby'));
+		add_filter('request', array(&$this, 'default_column_orderby'));
 
 	}
 
@@ -113,12 +115,35 @@ class Rd_Event extends Rd_Cms{
 	}
 
 	function manage_edit_rd_event_sortable_columns($columns) {
-		d($columns);
-		return array(
-			'start_date_time' => 'start_date_time',
-			'venue_text' => 'venue_text',
-			'rd_project_id' => 'rd_project_id',
+		return array_merge(
+			$columns,
+			array(
+				'start_date_time' => 'start_date_time',
+				'venue_text_en' => 'venue_text_en',
+				'rd_project_id' => 'rd_project_id',
+			)
 		);
+	}
+
+	function column_orderby($vars){
+		if (isset($vars['orderby']) && in_array($vars['orderby'], array('start_date_time', 'venue_text'))) {
+			$vars = array_merge($vars, array(
+				'meta_key' => $vars['orderby'],
+				'orderby' => 'meta_value_num',
+			));
+		}
+		return $vars;
+	}
+
+	function default_column_orderby($vars){
+		$screen = get_current_screen();
+		if ($screen->base == 'edit' && $screen->post_type == 'rd_event'){
+			if (!isset( $vars['orderby'])){
+				$vars['meta_key'] = 'start_date_time';
+				$vars['orderby'] = 'meta_value_num';
+			}
+		}
+		return $vars;
 	}
 
 }
